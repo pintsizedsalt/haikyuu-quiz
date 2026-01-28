6e2346317ac4586d3abfea506bf387eac6ad94bc
aljon <aljonkenfernandez36@gmail.com>
2026-01-28 19:59:42 +0800

    Chore: Fix video loop haikyuu in welcome page

--- resources/views/welcome-quiz.blade.php
+++ resources/views/welcome-quiz.blade.php
@@ -7,28 +7,26 @@
 @endpush
 
 @section('content')
-<div class="welcome-page">
+    <div class="welcome-page">
+        <video autoplay muted loop playsinline class="bg-video">
+            <source src="{{ asset('videos/haikyuu movie.mp4') }}?v=1" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
 
-    <video autoplay muted loop class="bg-video">
-        <source src="{{ asset('videos/haikyuuloop.mp4') }}" type="video/mp4">
-    </video>
+        <div class="overlay"></div>
 
-    <div class="overlay"></div>
+        <div class="content">
+            <h1>Welcome to my Haikyuu Quiz!</h1>
+            <p>Let’s have fun and see if you really know Haikyuu!</p>
 
-    <div class="content">
-        <h1>Welcome to my Haikyuu Quiz!</h1>
-        <p>Let’s have fun and see if you really know Haikyuu!</p>
+            <div class="buttons"> <button class="star-btn" onclick="window.open('https://github.com/yourusername', '_blank')">
+                    Follow me on GitHub
+                </button>
 
-        <button class="star-btn"
-            onclick="window.open('https://github.com/yourusername', '_blank')">
-            Follow me on GitHub
-        </button>
-
-        <button class="star-btn"
-            onclick="window.location.href='{{ url('quiz/start') }}'">
-            Start Quiz
-        </button>
+                <button class="star-btn" onclick="window.location.href='{{ url('quiz/start') }}'">
+                    Start Quiz
+                </button>
+            </div>
+        </div>
     </div>
-
-</div>
 @endsection
